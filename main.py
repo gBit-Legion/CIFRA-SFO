489f7149ac230c94b5c20971a75b5c9184cc26c0
johnseeena <82346607+johnseeena@users.noreply.github.com>
2023-09-30 22:15:42 +0300

    some updates

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask('Cheburussia')
+app = Flask(__name__)
 
 @app.route("/")
 def hello_page():
-    return "<p>Hello, my Dear friends!</p>"
+    return render_template('index.html')
 
+@app.route("/stat")
+async def get_data():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
